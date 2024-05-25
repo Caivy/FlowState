f272336da50387d65b294cde1eb08235b5ab9458
Caivy <samnangkhath5@gmail.com>
2024-05-25 08:22:23 +0700

    Clean up the previous Implementation and Put it in a Function called Timers_In_Seconds
    Now take all of that code and put it into a function called
    timer_in_seconds where the function take in user_time in sec then clock
    start and end and return std::chrono::duration<double> type.

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,23 @@
 using namespace std;
 using namespace std::chrono_literals;
 
-// void timer(time_duration) {
-//
-// }
+std::chrono::duration<double> timer_in_seconds(int secs) {
+
+  // Now we start the monotonic clock that will never be adjusted and count it as start
+  const auto start = std::chrono::steady_clock::now();
+
+  // we use the sleep function to sleep the allocated time by user
+  sleep(secs);
+
+  // Now we end the monotonic clock and count it as end
+  const auto end{std::chrono::steady_clock::now()};
+
+  // This is to output the durations (secs) of the end and start;
+  const std::chrono::duration<double> elapsed_seconds{end - start};
+
+  return elapsed_seconds;
+
+}
 
 int main (int argc, char *argv[]) {
   int user_time = 0;
@@ -17,20 +31,8 @@ int main (int argc, char *argv[]) {
   std::cout << "Start Timer: ";
   cin >> user_time;
 
-  // Now we start the monotonic clock that will never be adjusted and count it as start
-  const auto start = std::chrono::steady_clock::now();
-
-  // we use the sleep function to sleep the allocated time by user
-  sleep(user_time);
-
-  // Now we end the monotonic clock and count it as end
-  const auto end{std::chrono::steady_clock::now()};
-
-  // This is to output the durations (secs) of the end and start;
-  const std::chrono::duration<double> elapsed_seconds{end - start};
-
   // Now we just print the time that user sleep
-  std::cout << elapsed_seconds.count() << '\n';
+  std::cout << timer_in_seconds(user_time).count() << '\n';
 
   return 0;
 }
